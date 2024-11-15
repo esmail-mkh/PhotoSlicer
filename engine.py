c33a9c10a9d3996f3965a33af04ef28ab82593b2
Esmaeil <77193962+esmail-mkh@users.noreply.github.com>
2024-11-15 10:47:19 +0330

    Update engine.py
    updated to v3.5

--- engine.py
+++ engine.py
@@ -1,6 +1,13 @@
 from math import ceil
-from PIL import Image, ImageFile
+from PIL import Image
 import os, glob
+from PIL import ImageFile
+import zipfile
+import shutil
+from pillow_heif import register_avif_opener
+import re
+
+register_avif_opener()
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 Image.MAX_IMAGE_PIXELS = None
@@ -12,9 +19,11 @@ def image_width_changer(image_paths, new_width: int):
     for image_path in image_paths:
         with Image.open(image_path) as image:
             width, height = image.size
-            new_height = int(new_width * height / width)
-            resized_image = image.resize((new_width, new_height))
-            new_images.append(resized_image)
+            if width > 0 and height > 0 and new_width > 0:
+                new_height = int(new_width * height / width)
+                if new_height > 0:
+                    resized_image = image.resize((new_width, new_height))
+                    new_images.append(resized_image)
 
     return new_images
 
@@ -61,19 +70,32 @@ def get_concat_v_image(images, save_format):
     return dst
 
 
-def slicer(image, saveFormat, slicesCount, saveQuality, mode, current_date, saveDirectory=None):
+def slicer(image, saveFormat, slicesCount, saveQuality, mode, current_date, saveDirectory=None, isZip=False):
     try:
         width, height = image.size
         slices = height / ceil(slicesCount)
         last = 0
         # Create the save directory if it does not exist
 
+        # Determine the base save path and folder name
+        base_folder = "./Results"
         if mode == 'single':
-            save_path = os.path.join("./Results", saveDirectory or "")
+            folderName = saveDirectory or "folderName"
+            save_path = os.path.join(base_folder, folderName)
         elif mode == 'multi':
-            save_path = os.path.join("./Results", current_date, saveDirectory or "")
+            folderName = saveDirectory or current_date
+            save_path = os.path.join(base_folder, current_date, folderName)
         else:
             raise ValueError("Invalid mode. Mode should be either 'single' or 'multi'.")
+
+        # Check for existing folders and zip files and add numbering if necessary
+        counter = 0
+        original_save_path = save_path
+        while os.path.exists(save_path) or os.path.exists(f"{save_path}.zip"):
+            counter += 1
+            save_path = f"{original_save_path} ({counter})"
+
+        # Create the final save directory
         os.makedirs(save_path, exist_ok=True)
 
         for i in range(1, ceil(slicesCount) + 1):
@@ -83,13 +105,28 @@ def slicer(image, saveFormat, slicesCount, saveQuality, mode, current_date, save
             filepath = os.path.join(save_path, filename)
 
             if saveFormat == "jpg":
-                res.save(filepath, quality=saveQuality)
+                res.save(filepath, quality=saveQuality, optimize=True, progressive=True)
             elif saveFormat == "png":
                 res.save(filepath)
             elif saveFormat == "webp":
-                res.save(filepath, quality=saveQuality, method=6)
+                res.save(filepath, quality=saveQuality, method=6, optimize=True, progressive=True)
 
             res.close()
+
+        if isZip:
+            folderName = os.path.basename(save_path)
+            if mode == 'single':
+                zipFilePath = os.path.join(os.path.dirname(save_path), f"{folderName}.zip")
+            elif mode == 'multi':
+                zipFilePath = os.path.join("./Results", current_date, f"{folderName}.zip")
+
+            with zipfile.ZipFile(zipFilePath, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as zip_file:
+                for file in os.listdir(save_path):
+                    file_path = os.path.join(save_path, file)
+                    if os.path.isfile(file_path):
+                        zip_file.write(file_path, arcname=file)  # Only add the file without directory structure
+
+            shutil.rmtree(save_path)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -101,14 +138,19 @@ def fast_scandir(dirname):
     subfolders = [f.path for f in os.scandir(dirname) if f.is_dir()]
     return subfolders
 
+
 def getAllImagesDirectory(imagesPath):
     imagesLocations = []
-    for i in ["jpg", "jpeg", "png", "webp"]:
+    for i in ["jpg", "jpeg", "png", "webp", "avif"]:
         imagesLocations.extend(glob.glob(fr"{imagesPath}/*.{i}"))
-    return sorted(imagesLocations)
+    # مرتب‌سازی بر اساس شماره در نام فایل
+    return sorted(
+        imagesLocations,
+        key=lambda x: int(re.search(r'\d+', os.path.basename(x)).group()) if re.search(r'\d+', os.path.basename(x)) else 0
+    )
 
 
-def mergerImages(mode, newWidth, isChecked, imagePaths, saveFormat, SaveQuality, saveDirectory, heightLimit, current_date):
+def mergerImages(mode, newWidth, isChecked, imagePaths, saveFormat, SaveQuality, saveDirectory, heightLimit, current_date, is_zip):
     images = getAllImagesDirectory(imagePaths)
     if len(images) > 0:
         if isChecked:
@@ -118,7 +160,7 @@ def mergerImages(mode, newWidth, isChecked, imagePaths, saveFormat, SaveQuality,
         else:
             result = get_concat_v(images, saveFormat)
         SlicerCount = (int(result.height) / heightLimit)
-        slicer(result, saveFormat, SlicerCount,SaveQuality, mode, current_date, saveDirectory)
+        slicer(result, saveFormat, SlicerCount,SaveQuality, mode, current_date, saveDirectory, is_zip)
         return True
     else:
         return False
